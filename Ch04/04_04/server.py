976c899a028dd59b88bbdb89d4926465ca6014a7
Miki Tebeka <miki@353solutions.com>
2024-10-16 14:09:30 +0000

    04_04

--- Ch04/04_04/server.py
+++ Ch04/04_04/server.py
@@ -1,10 +1,24 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+
+from http import HTTPStatus
 
 app = FastAPI()
 
 
 class FreqError(Exception):
     pass
+
+
+@app.exception_handler(FreqError)
+async def freq_error_handler(request: Request, exc: FreqError):
+    return JSONResponse(
+        status_code=HTTPStatus.BAD_REQUEST,
+        content={'error': str(exc)},  # Security risk
+        headers={
+            'X-Freq-Text': request.query_params.get('text'),
+        }
+    )
 
 
 def char_freq(text: str):
